1dacfcf7f63c577614727ec4b0125a9e3ec1edbb
fikri ramadhani <fikryramadhani05@gmail.com>
2023-11-13 15:04:55 +0700

    Update Template

---
+++ resources/views/pages/kategori/index.blade.php
@@ -0,0 +1,189 @@
+@extends('layouts.main')
+@section('konten')
+    <div class="main-panel">
+        <div class="content-wrapper">
+            <div class="col-lg-12 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <h4 class="card-title">kategori Resep</h4>
+                                <p class="card-description">
+                                    Daftar Kategori Resep Makanan
+                                </p>
+                            </div>
+                            <div class="col text-right">
+                                <div class="btn btn-warning">Tambah Data</div>
+                            </div>
+                        </div>
+
+                        <div class="table-responsive pt-3">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>
+                                            #
+                                        </th>
+                                        <th>
+                                            First name
+                                        </th>
+                                        <th>
+                                            Progress
+                                        </th>
+                                        <th>
+                                            Amount
+                                        </th>
+                                        <th>
+                                            Deadline
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            1
+                                        </td>
+                                        <td>
+                                            Herman Beck
+                                        </td>
+                                        <td>
+                                            <div class="progress">
+                                                <div class="progress-bar bg-success" role="progressbar" style="width: %"
+                                                    aria-valuenow="25" aria-valuemin="0" aria-valuemax="1000"></div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            $ 77.99
+                                        </td>
+                                        <td>
+                                            May 15, 2015
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            2
+                                        </td>
+                                        <td>
+                                            Messsy Adam
+                                        </td>
+                                        <td>
+                                            <div class="progress">
+                                                <div class="progress-bar bg-danger" role="progressbar" style="width: 75%"
+                                                    aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            $245.30
+                                        </td>
+                                        <td>
+                                            July 1, 2015
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            3
+                                        </td>
+                                        <td>
+                                            John Richards
+                                        </td>
+                                        <td>
+                                            <div class="progress">
+                                                <div class="progress-bar bg-warning" role="progressbar" style="width: 90%"
+                                                    aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            $138.00
+                                        </td>
+                                        <td>
+                                            Apr 12, 2015
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            4
+                                        </td>
+                                        <td>
+                                            Peter Meggik
+                                        </td>
+                                        <td>
+                                            <div class="progress">
+                                                <div class="progress-bar bg-primary" role="progressbar" style="width: 50%"
+                                                    aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            $ 77.99
+                                        </td>
+                                        <td>
+                                            May 15, 2015
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            5
+                                        </td>
+                                        <td>
+                                            Edward
+                                        </td>
+                                        <td>
+                                            <div class="progress">
+                                                <div class="progress-bar bg-danger" role="progressbar" style="width: 35%"
+                                                    aria-valuenow="35" aria-valuemin="0" aria-valuemax="100"></div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            $ 160.25
+                                        </td>
+                                        <td>
+                                            May 03, 2015
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            6
+                                        </td>
+                                        <td>
+                                            John Doe
+                                        </td>
+                                        <td>
+                                            <div class="progress">
+                                                <div class="progress-bar bg-info" role="progressbar" style="width: 65%"
+                                                    aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            $ 123.21
+                                        </td>
+                                        <td>
+                                            April 05, 2015
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            7
+                                        </td>
+                                        <td>
+                                            Henry Tom
+                                        </td>
+                                        <td>
+                                            <div class="progress">
+                                                <div class="progress-bar bg-warning" role="progressbar" style="width: 20%"
+                                                    aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            $ 150.00
+                                        </td>
+                                        <td>
+                                            June 16, 2015
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\ResepController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +20,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+
+Route::get('/dashboard', [HomeController::class, 'index'])->name('home');
+
+Route::prefix('dashboard')->middleware('auth')->group(
+    function () {
+        Route::get('/', function () {
+            return view('layouts.home');
+        });
+        // route kategori
+        Route::get('resep-kategori', [KategoriController::class, 'index'])->name('kategori.index');
+        Route::get('resep-kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
+        Route::get('resep-kategori/{id}/show', [KategoriController::class, 'show'])->name('kategori.show');
+        Route::get('resep-kategori/{id}/edit', [KategoriController::class, 'edit'])->name('kategori.edit');
+        Route::patch('resep-kategori/{id}', [KategoriController::class, 'update'])->name('kategori.update');
+        Route::post('resep-kategori/store', [KategoriController::class, 'store'])->name('kategori.store');
+        Route::delete('resep-kategori/destroy/{id}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
+
+
+        // resep Controller
+        Route::get('/resep-masakan', [ResepController::class, 'index'])->name('resep.index');
+        Route::get('/resep-masakan/create', [ResepController::class, 'create'])->name('resep.create');
+        Route::get('/resep-masakan/{id}/show', [ResepController::class, 'show'])->name('resep.show');
+        Route::get('/resep-masakan/{id}/edit', [ResepController::class, 'edit'])->name('resep.edit');
+        Route::patch('/resep-masakan/{id}', [ResepController::class, 'update'])->name('resep.update');
+        Route::post('/resep-masakan/store', [ResepController::class, 'store'])->name('resep.store');
+        Route::delete('/resep-masakan/destroy/{id}', [ResepController::class, 'destroy'])->name('resep.destroy');
+    }
+);
